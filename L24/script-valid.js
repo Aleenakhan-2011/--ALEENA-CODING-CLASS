50491c6355c8427c4ed79c9c256082d95bbac412
Aleenakhan-2011 <abdulkalamkhan57@gmail.com>
2025-10-01 21:05:44 +0530

    L24 p1

---
+++ L24/script-valid.js
@@ -0,0 +1,28 @@
+function validate(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const age = document.getElementById("age").value;
+    const msgBox = document.getElementById("message");
+
+    let message = '';
+
+    if (email === '') {
+        message = 'Please enter an email.<br>';
+        msgBox.style.color = 'red';
+    } else if (password === '') {
+        message = 'Password must be at least 8 characters.<br>';
+        msgBox.style.color = 'red';
+    } else if (age === '') {
+        message = 'Age must be between 12 and 50.<br>';
+        msgBox.style.color = 'red';
+    } 
+    
+    else {
+        message = 'Login successfully!'; 
+        msgBox.style.color = 'green';
+    }
+    
+    msgBox.innerHTML = message;
+} 
