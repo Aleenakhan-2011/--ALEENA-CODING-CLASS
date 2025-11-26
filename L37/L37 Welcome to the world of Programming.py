e9c34ab4e841539853913fbf65c8afb61b271dfb
Aleenakhan-2011 <abdulkalamkhan57@gmail.com>
2025-11-26 16:12:49 +0530

    L37 p1

---
+++ L37/L37 Welcome to the world of Programming.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of Programming!")
+
+#Print a number
+print(42)
+
+#Next line after print
+print("Hello Python \n")
+
+import keyword as kw
+
+#Print all keywords present in Python
+print("The keywords present in Python are....")
+print(kw.kwlist)
