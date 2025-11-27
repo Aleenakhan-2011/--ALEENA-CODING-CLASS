68cc2778b0e4d81941042428741641dd30865c0d
Aleenakhan-2011 <abdulkalamkhan57@gmail.com>
2025-11-27 20:53:09 +0530

    L38 hw

---
+++ L38/L38 Hw Number Swapping.py
@@ -0,0 +1,22 @@
+#Take input values from the user
+x = 63
+y = 27
+z = 15
+
+print("Before Swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+print()
+
+#Swapping
+temp = x
+x = y
+y = z
+z = temp
+
+#Displaying the result after swapping
+print("Value of x after swapping:", x)
+print("Value of y after swapping:", y)
+print("Value of z after swapping:", z)
