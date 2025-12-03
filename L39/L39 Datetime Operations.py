2ae3c0410690ff7c21d4f1f84a0ae907f320b86a
Aleenakhan-2011 <abdulkalamkhan57@gmail.com>
2025-12-03 16:08:29 +0530

    L39

---
+++ L39/L39 Datetime Operations.py
@@ -0,0 +1,12 @@
+import datetime
+
+# using now() to get current time
+current_datetime = datetime.datetime.now() 
+
+# printing value of now
+print("Time now at my place is: ", end="")
+print(current_datetime)
+
+import calendar
+
+print("\n", calendar.calendar(2025))
